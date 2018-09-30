1148a1d31c6eba82862daf53b3788227df39f74e
proyectosbeta <josego85@gmail.com>
2018-09-30 19:49:26 -0400

    Se realizan los calculos para el test howard gardner

--- fuentes/application/controllers/TestHowardGardner.php
+++ fuentes/application/controllers/TestHowardGardner.php
@@ -18,16 +18,24 @@ class TestHowardGardner extends SMG_Controller {
 
 	public function encuesta()
 	{
-		$datos = array(
-			'courseid' => 1,
-			'userid' => 1,
-			'multipleintelligencedatetime' => $this->fecha_hora_actual,
-		);
+		$datos = array();
+
+		// Se agrega en el array los valores de las preguntas.
 		$limite_preguntas = 35;
 		for($indice = 1; $indice <= $limite_preguntas; $indice++){
 			$preg = "p" . $indice;
 			$datos[$preg] = $this->input->post($preg, true);
 		}
+
+		// Obtener el array de las evaluaciones de los test.
+		$array_evaluaciones_test = $this->evaluar_test($datos, $limite_preguntas);
+
+		$datos['courseid'] = 1;
+		$datos['userid'] = 1;
+		$datos['multipleintelligencedatetime'] = $this->fecha_hora_actual;
+
+		// Se unen los array de las preguntas con el array de las evaluaciones test.
+		$datos = array_merge($datos, $array_evaluaciones_test);
 
 		// Insertar la encuenta test Howard Gardner en la base de datos.
 		if($this->testHowardGardner->insertar_howard_gardner($datos)){
@@ -36,5 +44,55 @@ class TestHowardGardner extends SMG_Controller {
 			 echo "No inserto correctamente.";
 		}
 		$this->load->view('test_howard_gardner');
+	}
+
+	/**
+	 * @method evaluar_test
+	 */
+	private function evaluar_test($p_datos, $p_limite_preguntas){
+		$array_calculos = array(
+			'linguisticaverbal' => 0,
+			'logicamatematica' => 0,
+			'visualespacial' => 0,
+			'kinestesicacorporal' => 0,
+			'musicalritmica' => 0,
+			'intrapersonal' => 0,
+			'interpersonal' => 0
+		);
+
+		for($pregunta = 1; $pregunta <= $p_limite_preguntas; $pregunta++){
+			$preg = "p" . ($pregunta);
+
+			// linguisticaverbal
+			if($pregunta == 9 || $pregunta == 10 || $pregunta == 17 || $pregunta == 22
+		      || $pregunta == 30){
+				  $array_calculos['linguisticaverbal'] += $p_datos[$preg];
+			}else if($pregunta == 5 || $pregunta == 7 || $pregunta == 15 || $pregunta == 20
+		      || $pregunta == 25){
+				  // logicamatematica
+				  $array_calculos['logicamatematica'] += $p_datos[$preg];
+			}else if($pregunta == 1 || $pregunta == 11 || $pregunta == 14 || $pregunta == 23
+		      || $pregunta == 27){
+				  // visualespacial
+				  $array_calculos['visualespacial'] += $p_datos[$preg];
+			}else if($pregunta == 8 || $pregunta == 16 || $pregunta == 19 || $pregunta == 21
+		      || $pregunta == 29){
+				  // kinestesicacorporal
+				  $array_calculos['kinestesicacorporal'] += $p_datos[$preg];
+			}else if($pregunta == 3 || $pregunta == 4 || $pregunta == 13 || $pregunta == 24
+		      || $pregunta == 28){
+				  // musicalritmica
+				  $array_calculos['musicalritmica'] += $p_datos[$preg];
+			}else if($pregunta == 2 || $pregunta == 6 || $pregunta == 26 || $pregunta == 31
+		      || $pregunta == 33){
+				  // intrapersonal
+				  $array_calculos['intrapersonal'] += $p_datos[$preg];
+			}else if($pregunta == 12 || $pregunta == 18 || $pregunta == 32 || $pregunta == 34
+		      || $pregunta == 35){
+				  // interpersonal
+				  $array_calculos['interpersonal'] += $p_datos[$preg];
+			}
+		}
+		return $array_calculos;
 	}
 }
--- fuentes/application/views/test_howard_gardner.php
+++ fuentes/application/views/test_howard_gardner.php
@@ -21,8 +21,8 @@
                 <label for="p1" class="control-label">1. Prefiero hacer un mapa que explicarle a alguien como tiene que llegar.</label>
                 <select class="form-control" name="p1" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -30,8 +30,8 @@
                 <label for="p2" class="control-label">2. Si estoy enojado(a) o contento (a) generalmente s&eacute; exactamente por qu&eacute;.</label>
                 <select class="form-control" name="p2" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -39,8 +39,8 @@
                 <label for="p3" class="control-label">3. S&eacute; tocar (o antes sab&iacute;a tocar) un instrumento musical.</label>
                 <select class="form-control" name="p3" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -48,8 +48,8 @@
                 <label for="p4" class="control-label">4. Asocio la m&uacute;sica con mis estados de &aacute;nimo.</label>
                 <select class="form-control" name="p4" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -57,8 +57,8 @@
                 <label for="p5" class="control-label">5. Puedo sumar o multiplicar mentalmente con mucha rapidez.</label>
                 <select class="form-control" name="p5" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -67,8 +67,8 @@
 relaci&oacute;n a sentimientos parecidos.</label>
                 <select class="form-control" name="p6" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -76,8 +76,8 @@ relaci&oacute;n a sentimientos parecidos.</label>
                 <label for="p7" class="control-label">7. Me gusta trabajar con calculadoras y computadores.</label>
                 <select class="form-control" name="p7" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -85,8 +85,8 @@ relaci&oacute;n a sentimientos parecidos.</label>
                 <label for="p8" class="control-label">8. Aprendo r&aacute;pido a bailar un ritmo nuevo.</label>
                 <select class="form-control" name="p8" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -94,8 +94,8 @@ relaci&oacute;n a sentimientos parecidos.</label>
                 <label for="p9" class="control-label">9. No me es dif&iacute;cil decir lo que pienso en el curso de una discusi&oacute;n o debate.</label>
                 <select class="form-control" name="p9" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -103,8 +103,8 @@ relaci&oacute;n a sentimientos parecidos.</label>
                 <label for="p10" class="control-label">10. Disfruto de una buena charla, discurso o serm&oacute;n.</label>
                 <select class="form-control" name="p10" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -112,8 +112,8 @@ relaci&oacute;n a sentimientos parecidos.</label>
                 <label for="p11" class="control-label">11. Siempre distingo el norte del sur, est&eacute; donde est&eacute;.</label>
                 <select class="form-control" name="p11" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -121,8 +121,8 @@ relaci&oacute;n a sentimientos parecidos.</label>
                 <label for="p12" class="control-label">12. Me gusta reunir grupos de personas en una fiesta o en un evento especial.</label>
                 <select class="form-control" name="p12" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -130,8 +130,8 @@ relaci&oacute;n a sentimientos parecidos.</label>
                 <label for="p13" class="control-label">13. La vida me parece vac&iacute;a sin m&uacute;sica.</label>
                 <select class="form-control" name="p13" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -140,8 +140,8 @@ relaci&oacute;n a sentimientos parecidos.</label>
 instrumentos.</label>
                 <select class="form-control" name="p14" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -149,8 +149,8 @@ instrumentos.</label>
                 <label for="p15" class="control-label">15. Me gusta hacer rompecabezas y entretenerme con juegos electr&oacute;nicos.</label>
                 <select class="form-control" name="p15" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -158,8 +158,8 @@ instrumentos.</label>
                 <label for="p16" class="control-label">16. Me fue f&aacute;cil aprender a andar en bicicleta (o patines).</label>
                 <select class="form-control" name="p16" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -167,8 +167,8 @@ instrumentos.</label>
                 <label for="p17" class="control-label">17. Me enojo cuando oigo una discusi&oacute;n o una afirmaci&oacute;n que parece il&oacute;gica.</label>
                 <select class="form-control" name="p17" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -176,8 +176,8 @@ instrumentos.</label>
                 <label for="p18" class="control-label">18. Soy capaz de convencer a otros que sigan mis planes.</label>
                 <select class="form-control" name="p18" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -185,8 +185,8 @@ instrumentos.</label>
                 <label for="p19" class="control-label">19. Tengo buen sentido de equilibrio y coordinaci&oacute;n.</label>
                 <select class="form-control" name="p19" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -195,8 +195,8 @@ instrumentos.</label>
 facilidad que otros.</label>
                 <select class="form-control" name="p20" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -204,8 +204,8 @@ facilidad que otros.</label>
                 <label for="p21" class="control-label">21. Me gusta construir modelos (o hacer esculturas).</label>
                 <select class="form-control" name="p21" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -213,8 +213,8 @@ facilidad que otros.</label>
                 <label for="p22" class="control-label">22. Tengo agudeza para encontrar el significado de las palabras.</label>
                 <select class="form-control" name="p22" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -222,8 +222,8 @@ facilidad que otros.</label>
                 <label for="p23" class="control-label">23. Puedo mirar un objeto de una manera y con la misma facilidad verlo.</label>
                 <select class="form-control" name="p23" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -231,8 +231,8 @@ facilidad que otros.</label>
                 <label for="p24" class="control-label">24. Con frecuencia hago la conexi&oacute;n entre una pieza de m&uacute;sica y alg&uacute;n evento de mi vida.</label>
                 <select class="form-control" name="p24" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -240,8 +240,8 @@ facilidad que otros.</label>
                 <label for="p25" class="control-label">25. Me gusta trabajar con n&uacute;meros y figuras.</label>
                 <select class="form-control" name="p25" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -249,8 +249,8 @@ facilidad que otros.</label>
                 <label for="p26" class="control-label">26. Me gusta sentarme silenciosamente y reflexionar sobre mis sentimientos &iacute;ntimos.</label>
                 <select class="form-control" name="p26" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -258,8 +258,8 @@ facilidad que otros.</label>
                 <label for="p27" class="control-label">27. Con s&oacute;lo mirar la forma de construcciones y estructuras me siento a gusto.</label>
                 <select class="form-control" name="p27" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -267,8 +267,8 @@ facilidad que otros.</label>
                 <label for="p28" class="control-label">28. Me gusta tararear, silbar y cantar en la ducha o cuando estoy sola.</label>
                 <select class="form-control" name="p28" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -276,8 +276,8 @@ facilidad que otros.</label>
                 <label for="p29" class="control-label">29. Soy bueno(a) para el atletismo.</label>
                 <select class="form-control" name="p29" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -285,8 +285,8 @@ facilidad que otros.</label>
                 <label for="p30" class="control-label">30. Me gusta escribir cartas detalladas a mis amigos.</label>
                 <select class="form-control" name="p30" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -294,8 +294,8 @@ facilidad que otros.</label>
                 <label for="p31" class="control-label">31. Generalmente me doy cuenta de la expresi&oacute;n que tengo en la cara.</label>
                 <select class="form-control" name="p31" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -303,8 +303,8 @@ facilidad que otros.</label>
                 <label for="p32" class="control-label">32. Me doy cuenta de las expresiones en la cara de otras personas.</label>
                 <select class="form-control" name="p32" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -312,8 +312,8 @@ facilidad que otros.</label>
                 <label for="p33" class="control-label">33. Me mantengo "en contacto" con mis estados de &aacute;nimo. No me cuesta identificarlos.</label>
                 <select class="form-control" name="p33" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -321,8 +321,8 @@ facilidad que otros.</label>
                 <label for="p34" class="control-label">34. Me doy cuenta de los estados de &aacute;nimo de otros.</label>
                 <select class="form-control" name="p34" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
@@ -330,8 +330,8 @@ facilidad que otros.</label>
                 <label for="p35" class="control-label">35. Me doy cuenta bastante bien de lo que otros piensan de m&iacute;.</label>
                 <select class="form-control" name="p35" required>
                     <option value=""></option>
-                    <option value="1">(v) Verdadero</option>
-                    <option value="0">(f) Falso</option>
+                    <option value="1">Verdadero</option>
+                    <option value="0">Falso</option>
                 </select>
             </div>
 
